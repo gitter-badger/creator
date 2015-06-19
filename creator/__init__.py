22a93c652e7fa1cfbbed1a4a83d9c8674337cb05
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-06-19 17:09:32 +0200

    initial commit

---
+++ creator/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2015 Niklas Rosenstein
+# All rights reserved.
+"""
+Creator - software build automation tool
+========================================
+
+Visit https://github.com/NiklasRosenstein/py-creator/wiki.
+"""
+
+__author__ = 'Niklas Rosenstein <rosensteinniklas(at)gmail.com>'
+__version__ = '0.0.1'
+__url__ = 'https://github.com/NiklasRosenstein/py-creator/wiki'
+
+import sys
+if sys.version_info[0] != 3:
+  raise EnvironmentError('Creator {0} requires Python 3'.format(__version__))
